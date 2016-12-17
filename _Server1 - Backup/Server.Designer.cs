a1bacb323a87650c81536c1eef2178e009b9ee50
Khoi Cao Minh <husky.uit.workshop@gmail.com>
2016-12-17 22:27:26 +0700

    Add files via upload

---
+++ _Server1 - Backup/Server.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace _Server1
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rtbTask = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rtbConnect = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbIP = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(243, 11);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(125, 48);
+            this.btnStart.TabIndex = 32;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(64, 39);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(150, 20);
+            this.tbPort.TabIndex = 31;
+            this.tbPort.Text = "13000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Quản lý tác vụ";
+            // 
+            // rtbTask
+            // 
+            this.rtbTask.Location = new System.Drawing.Point(15, 219);
+            this.rtbTask.Name = "rtbTask";
+            this.rtbTask.Size = new System.Drawing.Size(353, 136);
+            this.rtbTask.TabIndex = 29;
+            this.rtbTask.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Quản lý kết nối";
+            // 
+            // rtbConnect
+            // 
+            this.rtbConnect.Location = new System.Drawing.Point(15, 95);
+            this.rtbConnect.Name = "rtbConnect";
+            this.rtbConnect.Size = new System.Drawing.Size(353, 96);
+            this.rtbConnect.TabIndex = 27;
+            this.rtbConnect.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Port";
+            // 
+            // lbIP
+            // 
+            this.lbIP.AutoSize = true;
+            this.lbIP.Location = new System.Drawing.Point(12, 10);
+            this.lbIP.Name = "lbIP";
+            this.lbIP.Size = new System.Drawing.Size(61, 13);
+            this.lbIP.TabIndex = 25;
+            this.lbIP.Text = "Server IP is";
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 377);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rtbTask);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rtbConnect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbIP);
+            this.Name = "Server";
+            this.Text = "Backup";
+            this.Load += new System.EventHandler(this.Server_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox rtbTask;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rtbConnect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbIP;
+    }
+}
+
---
+++ _Server1 - Backup/SocketModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+namespace _Server1
+{
+	public class SocketModel
+	{
+		private Socket socket;
+		private byte[] bufferReceive;
+		private string remoteEndPoint;		
+		
+		public SocketModel(Socket s)
+		{
+			socket = s;
+            bufferReceive = new byte[100];
+		}
+		public SocketModel(Socket s,int length)
+		{
+			socket = s;
+            bufferReceive = new byte[length];
+		}		
+		//get the IP and port of connected client
+		public string GetRemoteEndpoint(){
+			string str = "";
+			try{
+				str = Convert.ToString(socket.RemoteEndPoint);
+				remoteEndPoint = str;			
+			}
+			catch (Exception e){
+				string str1 = "Error..... " + e.StackTrace;
+		        //Console.WriteLine(str1);
+		        str = "Socket is closed with " + remoteEndPoint;
+			}
+			return str;			
+		}
+        //receive data from client
+        public string ReceiveData()
+        {
+            //server just can receive data AFTER a connection is set up between server and client
+            string data = string.Empty;
+            try
+            {
+                int k = socket.Receive(bufferReceive);
+                data = Encoding.ASCII.GetString(bufferReceive);
+            }
+            catch (Exception e)
+            {
+                string str1 = "Error..... " + e.StackTrace;
+                //Console.WriteLine(str1);	      
+            }
+            return data;
+        }
+        //send data to client
+        public void SendData(string str)
+        {
+            try
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes(str);
+                socket.Send(buffer);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+        }
+        //close sockket
+        public void CloseSocket(){
+			socket.Close();
+		}		
+
+	}
+}
