f7035a7b7eb66904ece23693316d7f76f6df55ab
Khoi Cao Minh <husky.uit.workshop@gmail.com>
2016-12-17 22:26:56 +0700

    Add files via upload

---
+++ _Server1/Server1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+namespace _Server1
+{
+    public partial class Server1 : Form
+    {
+        private TCPModel tcp;
+        //private List<SocketModel> MainskList;
+        private SocketModel skCln;
+        //private List<SocketModel> SubskList;
+        private int currentClient;
+        private object thisLock = new object();
+        private string myIP;
+        
+        public Server1()
+        {
+           
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();           
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartServer();
+            Thread t = new Thread(ServeClients);
+            t.Start();
+        }
+        public string GetIPsever()
+        {
+            string ipS = string.Empty;
+            IPAddress[] ipadd = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress x in ipadd)
+            {
+                if (IPAddress.Parse(x.ToString()).AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ipS = x.ToString();
+                }
+            }
+            lbIP.Text = "Server IP address is: " + ipS;
+            return ipS;
+        }
+
+        public void StartServer()
+        {
+            myIP = GetIPsever();
+            int port = int.Parse(tbPort.Text);
+            tcp = new TCPModel(myIP, port);
+            tcp.Listen();
+            btnStart.Enabled = false;          
+        }
+        public void ServeClients()
+        {                    
+            while(true)
+            {
+                ServeAClient();
+            }                 
+        }
+        public void Accept()
+        {
+            int status = -1;
+            Socket s = tcp.SetUpANewConnection(ref status);
+            skCln= new SocketModel(s);          
+            string str = skCln.GetRemoteEndpoint();
+            string str1 = "New connection from: " + str + "\n";
+            rtbConnect.AppendText(str1);
+        }       
+
+        public void ServeAClient()
+        {         
+            lock (thisLock)
+            {
+                Accept();                                           
+            }
+            Thread t = new Thread(Commmunication);
+            t.Start();
+        }
+
+        public void Commmunication()
+        {
+            while (true)
+            {
+                string input = skCln.ReceiveData();                
+                Calculate(input);            
+            }   
+        }
+        
+        public void Calculate(string input)
+        {
+            //long fac = 1;
+            //int index = int.Parse(input);
+            //if (index == 1 || index == 0)
+            //    return;
+            //if (index >= 2)
+            //{
+            //    for (int i = 2; i <= index; i++)
+            //        fac *= i;
+            //}
+            int k = int.Parse(input);
+            long fac = k * k;
+            skCln.SendData(fac.ToString());
+        }
+
+        private void Server_Load(object sender, EventArgs e)
+        {
+            Text = "Server 1";
+        }
+    }
+}
---
+++ _Server1/TCPModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+namespace _Server1
+{
+	public class TCPModel
+	{
+		private IPAddress ipAd_of_server;
+		private int port_to_listen;
+		private TcpListener tcpServer;	
+		
+		public TCPModel(string ip, int p)
+		{
+			ipAd_of_server = IPAddress.Parse(ip);
+			port_to_listen = p;
+			tcpServer = new TcpListener(ipAd_of_server, port_to_listen);			
+		}
+		//function to listen
+		public void Listen(){
+			try{
+				tcpServer.Start();
+			}
+			catch (Exception e){
+				Console.WriteLine("Error..... " + e.StackTrace);
+			}			
+		}
+		//accept a new connection
+		public Socket SetUpANewConnection(ref int status){
+			Socket socket = tcpServer.AcceptSocket();
+			status = 1;
+			return socket;
+		}
+		//shutdown server
+		public void Shutdown(){
+			//students should develop script to finish the job in progress by working sockets
+			tcpServer.Stop();			
+		}				
+	}
+}
